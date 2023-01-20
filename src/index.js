8dc15f19eb558187606692faf85281ddaa72516a
Rob Eastwood <re.web.accounts@protonmail.com>
2023-01-21 07:02:46 +1000

    remove commented line markers

--- src/index.js
+++ src/index.js
@@ -9,14 +9,13 @@ let links = [{
   description: 'Fullstack tutorial for GraphQL'
 }]
 
-// 2
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
   },
   Mutation: {
-    // 2
+
     post: (parent, args) => {
   
     let idCount = links.length
