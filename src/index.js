aef5c1abc25b781d43df67baf9cbb8d5fa3abf0d
Rob Eastwood <re.web.accounts@protonmail.com>
2023-01-22 06:47:35 +1000

    Refactor resolvers, query and mutations  to use prisma

--- src/index.js
+++ src/index.js
@@ -4,30 +4,22 @@ const path = require('path');
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
-
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany()
+    }
   },
   Mutation: {
 
-    post: (parent, args) => {
-  
-    let idCount = links.length
-
-       const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      })
     }
   },  
 }
